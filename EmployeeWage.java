4382313c990f764b983b7e9587094fcb45d0c934
Experiment2601 <aniketgavas18@gmail.com>
2022-10-25 12:32:30 +0530

    Employee_Wage_UC6

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -5,9 +5,12 @@ public class EmployeeWage {
     public static final int PART_TIME = 2;
     public static final int EMP_PER_HOUR_RATE = 50;
     public static final int NUMBER_OF_WORKING_DAYS = 20;
+    public static final int MAX_HRS_IN_MONTH = 20;
     int empHrs = 0;
     int empWage = 0;
     int totalEmpWage = 0;
+    int totalEmpHrs = 0;
+    int totalWorkingDays = 0;
 
 
     //Check Employee IS present Or Absent
@@ -92,6 +95,30 @@ public class EmployeeWage {
     }
 
 
+    //Calculating Wages till Number of Working Days or Total Working Hours per month is Reached
+
+    public void wagesTillTotalWorkingHoursPerMonth(){
+        while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUMBER_OF_WORKING_DAYS){
+            totalWorkingDays++;
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            switch (empCheck){
+                case FULL_TIME:
+                    empHrs = 8;
+                    break;
+                case PART_TIME:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+            }
+            totalEmpHrs += empHrs;
+            System.out.println("Day: "+totalWorkingDays+ "Employee Hour: "+empHrs);
+        }
+        int totalEmpWage = totalEmpHrs * EMP_PER_HOUR_RATE;
+        System.out.println("Total Employee wage: "+totalEmpWage);
+    }
+
+
     public static void main(String[] args) {
         EmployeeWage emp = new EmployeeWage();
         emp.empCheckPresentOrAbsent();
@@ -99,5 +126,6 @@ public class EmployeeWage {
         emp.addPartTimeAndEmpWage();
         emp.switchStatement();
         emp.empMonthlyWage();
+        emp.wagesTillTotalWorkingHoursPerMonth();
     }
 }
